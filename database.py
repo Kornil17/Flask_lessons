d0f1d271f999783cbce8b6f3d147cfebe0b83cac
Kornil17 <dimitra4@mail.ru>
2024-02-26 17:15:45 +0300

    added db functional

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, as_declarative, declared_attr
 from settings import Config
+from loguru import logger
+from datetime import datetime
+
+logger.add('db.logs', format='{time} {level} {message}', level='DEBUG', encoding='UTF8')
+
+
 
 data = Config.get_settings()
 engine = create_engine(
@@ -25,12 +31,74 @@ class Menu(AbsractModel):
     title: Mapped[str]
     url: Mapped[str]
 
+    def __iter__(self):
+        return self
 
+class Posts(AbsractModel):
+    __tablename__ = 'posts'
+
+    title: Mapped[str]
+    text: Mapped[str]
+    time: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+class WorkDb:
+    @classmethod
+    def get_menu(cls) -> list:
+        try:
+            with session() as conn:
+                data = conn.query(Menu).all()
+                return [dict(d.__dict__) for d in data]
+        except Exception as error:
+            logger.error(f"Got error by get menu -> {error}")
+    @classmethod
+    def insert_menu_data(cls, title, url) -> int:
+        try:
+            with session() as conn:
+                menu = Menu(title=title, url=url)
+                data = conn.add(menu)
+                conn.commit()
+                return menu.id
+        except Exception as error:
+            logger.error(f"Got error by indert menu -> {error}")
+
+    @classmethod
+    def add_post(cls, title, text) -> int:
+        try:
+            with session() as conn:
+                post = Posts(title=title, text=text)
+                data = conn.add(post)
+                conn.commit()
+                return True
+        except Exception as error:
+            logger.error(f"Got error by add post -> {error}")
+            return False
+    @classmethod
+    def get_post(cls, id) -> list:
+        try:
+            with session() as conn:
+                data = conn.query(Posts).filter_by(id=id).first()
+                return data
+        except Exception as error:
+            logger.error(f"Got error by get post -> {error}")
+    @classmethod
+    def get_all_posts(cls) -> dict:
+        try:
+            with session() as conn:
+                data = conn.query(Posts).all()
+                return [dict(d.__dict__) for d in data]
+
+        except Exception as error:
+            logger.error(f"Got error by get all post -> {error}")
+def main():
+    AbsractModel.metadata.create_all(engine)
+    logger.info(WorkDb.get_menu())
+    # for i in menu:
+    #     logger.info(WorkDb.insert_menu_data(title=i['name'], url=i['url']))
+    # logger.info(WorkDb.get_post(id=1))
+    # logger.info(WorkDb.get_all_posts())
 
 
 if __name__ == '__main__':
-    Menu()
-
+    main()
 
 
 
--- first_module/app.py
+++ first_module/app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, url_for, request, flash, get_flashed_messages, session, redirect, abort
 from settings import Config
+from database import WorkDb
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = Config.get_settings()['app']['key']
-menu = [
-    {"name": "Установка", "url": "install-flask"},
-    {"name": "Первое приложение", "url": "first-app"},
-    {"name": "Обратная связь", "url": "contact"}
-]
+# menu = [
+#     {"name": "Установка", "url": "install-flask"},
+#     {"name": "Добавить статью", "url": "addPost"},
+#     {"name": "Обратная связь", "url": "contact"}
+# ]
 @app.route('/')
 def main():
     print(url_for('main'))
-    return render_template('index.html', title='Flask Lessons', menu=menu)
+    return render_template('index.html', title='Flask Lessons', menu=WorkDb.get_menu(), posts=WorkDb.get_all_posts())
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
     if request.method == 'POST':
@@ -20,7 +22,7 @@ def contact():
             flash('Сообщение отправлено', category='success')
         else:
             flash('Ошибка отправки', category='error')
-    return render_template('contact.html', title='Обратная связь', menu=menu)
+    return render_template('contact.html', title='Обратная связь', menu=WorkDb.get_menu())
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
@@ -30,15 +32,38 @@ def login():
     elif request.method == "POST" and request.form['username'] == 'Dmitriy' and request.form['password'] == '123':
         session['userLogged'] = request.form['username']
         return redirect(url_for('profile', username=session['userLogged']))
-    return render_template('login.html', title='Авторизация', menu=menu)
+    return render_template('login.html', title='Авторизация', menu=WorkDb.get_menu())
 @app.route('/profile/<username>')
 def profile(username):
     if 'userLogged' not in session or session['userLogged'] != username:
         abort(401)
     return f'{username}'
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    if request.method == "POST":
+        if len(request.form['name']) >= 4:
+            res = WorkDb.add_post(request.form['name'], request.form['message'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=WorkDb.get_menu(), title="Добавление статьи")
+
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    data = WorkDb.get_post(id_post)
+    if not data:
+        abort(404)
+    title, post = data.title, data.text
+    return render_template('post.html', menu=WorkDb.get_menu(), title=title, post=post)
+
+
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page404.html', title='Страница не найдена', menu=menu), 404
+    return render_template('page404.html', title='Страница не найдена', menu=WorkDb.get_menu()), 404
 
 
 
